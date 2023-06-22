0375a3705799c0be8f0b8c0410653ca1f0831c73
chinjenadrian <adrian.chinjen28@gmail.com>
2023-06-22 11:29:46 +0800

    edited the app file

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ function App() {
         <p>
           Ian | Chi
         </p>
+        <p>
+          CHINJEN
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
