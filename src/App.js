fee0df50d4c95e5c22aeb9c90fdcfdd6e016e6b0
adrianchinjen <chinjenyhan28@gmail.com>
2023-06-22 14:10:38 +0800

    I have change the learning course link

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Docker CI/CD
         </a>
       </header>
     </div>
